d24644d279ef23f764ad2686ef32eaf0bb0d0f7c
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-10-30 11:54:35 +0100

    Updates imports for moved routes folder. Runs sequelize init to scaffold models, migrations and seeders folders. Organizes scaffolded folders.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const books = require('./routes/books');
+const books = require('./app/routes/books');
 const app = express();
 
 app
